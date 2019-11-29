10be9ce75cc029ef2d4203852982bc7623cebd8b
Cristi Constantin <cristi.constantin@sent.com>
2019-11-29 19:36:26 +0000

    Better config loading

--- tdb/config.py
+++ tdb/config.py
@@ -2,7 +2,8 @@
 Load local config files.
 """
 from pathlib import Path
-from typing import Any, Dict, Callable
+from runpy import run_path
+from typing import Dict, Callable
 
 KNOWN_FIELDS = ('keys', 'validate', 'transform')
 
@@ -13,16 +14,12 @@ def load_local_config(fname: str, model='tdb_config') -> Dict[str, Callable]:
     Only keys, filters, validation are used.
     """
     config: Dict[str, Callable] = {}
-    glob_vars: Dict[str, Any] = {}
-    local_vars: Dict[str, Any] = {}
 
     fpath = str(Path(fname).expanduser().resolve())
-    exec(open(fpath).read(), glob_vars, local_vars)
+    module = run_path(fpath)
 
-    if glob_vars.get(model):
-        config = glob_vars[model]
-    elif local_vars.get(model):
-        config = local_vars[model]
+    if module.get(model):
+        config = module[model]
     if not isinstance(config, dict):
         return {}
 
